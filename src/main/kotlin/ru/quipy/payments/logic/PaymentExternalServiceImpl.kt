97888f1650b9424e336b00fbf2162372654728d8
Maksim <petrov.maksim5@mail.ru>
2025-12-11 23:48:45 +0300

    fix: change overhead time

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -58,7 +58,7 @@ class PaymentExternalSystemAdapterImpl(
     )
 
     private val client = OkHttpClient.Builder().build()
-    private val processingOverheadMillis: Long = 20L
+    private val processingOverheadMillis: Long = 25L
 
     private val rateLimiter = SlidingWindowRateLimiter(rateLimitPerSec.toLong(), Duration.ofSeconds(1))
     private val ongoingRequestsLimiter = OngoingWindow(parallelRequests, fair = true)
