f99301ac53e9ad9bce3f24bdd98a11809e59bfa2
Maksim <petrov.maksim5@mail.ru>
2025-12-28 01:51:25 +0300

    try http1 back

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -97,7 +97,7 @@ class PaymentExternalSystemAdapterImpl(
         engine {
             dispatcher = Dispatchers.IO.limitedParallelism(16)
             pipelining = false
-            protocolVersion = java.net.http.HttpClient.Version.HTTP_2
+            protocolVersion = java.net.http.HttpClient.Version.HTTP_1_1
         }
         install(HttpTimeout) {
             requestTimeoutMillis = expectedProcessingTime.inWholeMilliseconds
