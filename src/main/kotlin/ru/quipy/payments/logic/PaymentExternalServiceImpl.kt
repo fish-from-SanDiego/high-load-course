a8ce00c7f615764d5eff415f74d6cd4b1b2a0544
Maksim <petrov.maksim5@mail.ru>
2025-12-27 23:59:03 +0300

    ...

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -11,6 +11,7 @@ import io.ktor.network.sockets.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.Channel
+import kotlinx.io.IOException
 import org.slf4j.LoggerFactory
 import ru.quipy.common.utils.CountingChannel
 import ru.quipy.common.utils.ExponentialBackoffDelayStrategy
@@ -95,7 +96,7 @@ class PaymentExternalSystemAdapterImpl(
     @OptIn(ExperimentalCoroutinesApi::class)
     private val client = HttpClient(Java) {
         engine {
-            dispatcher = Dispatchers.IO.limitedParallelism(30)
+            dispatcher = Dispatchers.IO.limitedParallelism(16)
             pipelining = true
             protocolVersion = java.net.http.HttpClient.Version.HTTP_2
 
@@ -342,7 +343,13 @@ class PaymentExternalSystemAdapterImpl(
             PaymentCallResult.RetryableFailure("Socket timeout")
         } catch (_: HttpRequestTimeoutException) {
             PaymentCallResult.RetryableFailure("Request timeout")
+        } catch (e: IOException) {
+            if (e.message?.contains("RST_STREAM") ?: false) {
+                return PaymentCallResult.RetryableFailure("RST_STREAM received")
+            }
+            throw e
         }
+
     }
 
 
