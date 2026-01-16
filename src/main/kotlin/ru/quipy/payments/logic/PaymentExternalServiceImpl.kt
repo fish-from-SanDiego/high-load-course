2d62c1a0dd01dc66a1e04ac393c5cf7d65cdb7e1
Maksim <petrov.maksim5@mail.ru>
2026-01-16 20:01:38 +0300

    ...

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -61,13 +61,13 @@ class PaymentExternalSystemAdapterImpl(
             parallelRequests / requestAverageProcessingTime.toKotlinDuration().toDouble(DurationUnit.SECONDS)
         )
 
-    private val paymentExecutor: ThreadPoolExecutor = Executors.newFixedThreadPool(32) as ThreadPoolExecutor
+    private val paymentExecutor: ThreadPoolExecutor = Executors.newFixedThreadPool(16) as ThreadPoolExecutor
     private val paymentDispatcher =
         paymentExecutor.asCoroutineDispatcher()
     private val paymentScope =
         CoroutineScope(SupervisorJob() + paymentDispatcher)
 
-    private val eventExecutor: ThreadPoolExecutor = Executors.newFixedThreadPool(16) as ThreadPoolExecutor
+    private val eventExecutor: ThreadPoolExecutor = Executors.newFixedThreadPool(4) as ThreadPoolExecutor
     private val eventDispatcher =
         eventExecutor.asCoroutineDispatcher()
 
@@ -330,7 +330,7 @@ class PaymentExternalSystemAdapterImpl(
     private suspend fun executeOnce(requestUrl: String): PaymentCallResult {
         return try {
             while (!outgoingRateLimiter.tick()) {
-                delay((75L..250L).random())
+                delay(20L)
             }
             val response = client.post(requestUrl)
             response.headers["Retry-After"]?.let {
