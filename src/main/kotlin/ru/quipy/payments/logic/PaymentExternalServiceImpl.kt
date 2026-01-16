2074eb7c17a310642773166c901773c66997eb5e
Maksim <petrov.maksim5@mail.ru>
2026-01-16 14:47:41 +0300

    ...

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -17,7 +17,6 @@ import ru.quipy.common.utils.CountingChannel
 import ru.quipy.common.utils.ExponentialBackoffDelayStrategy
 import ru.quipy.common.utils.LeakingBucketRateLimiter
 import ru.quipy.common.utils.RetryDelayStrategy
-import ru.quipy.common.utils.suspending.FixedWindowRateLimiter
 import ru.quipy.common.utils.suspending.OngoingWindow
 import ru.quipy.common.utils.suspending.SlidingWindowRateLimiter
 import ru.quipy.core.EventSourcingService
@@ -144,8 +143,7 @@ class PaymentExternalSystemAdapterImpl(
     }
 
 //    private val outgoingRateLimiter = SlidingWindowRateLimiter(rateLimitPerSec.toLong(), Duration.ofSeconds(1))
-//    private val outgoingRateLimiter = SlidingWindowRateLi miter(1110 / 2, Duration.ofMillis(500L))
-    private val outgoingRateLimiter = FixedWindowRateLimiter(rateLimitPerSec, 1, TimeUnit.SECONDS)
+    private val outgoingRateLimiter = SlidingWindowRateLimiter(1110 / 2, Duration.ofMillis(500L))
 
     private val incomingRateLimiterRate = expectedRps.toInt().coerceAtLeast(1)
     private val incomingRateLimiter = LeakingBucketRateLimiter(
