fca71cdee6a63a48fce80e75a4e1577b5f78f908
Maksim <petrov.maksim5@mail.ru>
2026-01-16 19:21:16 +0300

    modify delay to random

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.*
 import io.ktor.client.engine.java.*
-import io.ktor.client.engine.jetty.jakarta.Jetty
 import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
@@ -331,7 +330,7 @@ class PaymentExternalSystemAdapterImpl(
     private suspend fun executeOnce(requestUrl: String): PaymentCallResult {
         return try {
             while (!outgoingRateLimiter.tick()) {
-                delay(100L)
+                delay((75L..250L).random())
             }
             val response = client.post(requestUrl)
             response.headers["Retry-After"]?.let {
